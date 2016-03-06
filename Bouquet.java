3e443d586b943a12c0a2e811d116996a4c9a3e82
Mega343 <mega9594@Rambler.ru>
2016-03-06 22:06:20 +0200

    Added files via upload

---
+++ Bouquet.java
@@ -0,0 +1,12 @@
+package com.goit.gojavaonline.ht3;
+
+import java.util.List;
+
+public class Bouquet {
+
+    List<Astra> astra;
+    List<Rose> rose;
+    List<Romashka> romashkas;
+    List<Tulip> tulips;
+
+}
